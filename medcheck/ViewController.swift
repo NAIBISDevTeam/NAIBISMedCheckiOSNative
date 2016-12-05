6a11d7eb502ea5a6c1b836c912f71e5ecdb8fcf6
CreativeGuy2013 <lucacasonato@yahoo.com>
2016-12-05 13:59:56 +0100

    Initial Commit

---
+++ medcheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  medcheck
+//
+//  Created by Luca  Casonato on 05/12/2016.
+//  Copyright © 2016 naibis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
